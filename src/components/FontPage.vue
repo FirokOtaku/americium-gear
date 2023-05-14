824d8e51b16682dee902fe57df70007bce0ef162
Firok <firok@qq.com>
2023-05-14 20:29:54 +0800

    0.8.0

--- src/components/FontPage.vue
+++ src/components/FontPage.vue
@@ -1,3 +1,4 @@
+<!--suppress HtmlUnknownTarget, CssNoGenericFontName -->
 <style scoped>
 dl
 {
@@ -7,25 +8,249 @@ dl
 </style>
 
 <template>
-<div class="fill scroll max-height">
-	<div class="center-align middle-align" v-if="docContent == null">
-		<div class="loader extra yellow"></div>
-	</div>
-	<div class="large-text left-align" v-else>
-		<div class="markdown-body padding" v-html="docContent"></div>
-	</div>
-	<div v-for="i in 5" :key="i" class="large-space"></div>
+<div class="fill scroll max-height padding large-text">
+	<div>一些自己项目用到的字体</div>
+
+	<article class="blur">
+		<details>
+			<summary class="none">
+				<div class="row">
+					<div class="max">
+						<h5 class="text-shadow">汉仪文黑</h5>
+						<div>通用字体</div>
+					</div>
+					<i>arrow_drop_down</i>
+				</div>
+			</summary>
+
+			<div class="medium-space"></div>
+
+			<a class="chip medium-elevate small primary" href="https://www.hanyi.com.cn/productdetail?id=991" target="_blank">
+				<i class="small">home</i>
+				<span>官网</span>
+			</a>
+
+			<a class="chip medium-elevate small" href="https://www.hanyi.com.cn/faq-doc-1" target="_blank">
+				<i class="small">credit_card</i>
+				<span>个人免费 / 商用收费</span>
+			</a>
+
+			<h4>标签引入</h4>
+
+			<code lang="html" ref="domHywhUsage"></code>
+
+			<h4>直接下载</h4>
+
+			<div>按字重单独下载:</div>
+
+			<div class="grid">
+				<a class="s4 m4 l2" href="/astatine-gear/font-hywh/HYWenHei-35W.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> 35W</button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-hywh/HYWenHei-45W.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> 45W</button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-hywh/HYWenHei-55S.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> 55S</button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-hywh/HYWenHei-65W.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> 65W</button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-hywh/HYWenHei-75W.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> 75W</button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-hywh/HYWenHei-85W.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> 85W</button> </a>
+			</div>
+
+			<div class="space"></div>
+
+			<div>打包下载所有:</div>
+
+			<div class="grid">
+				<a class="s12 m12 l4" href="/astatine-gear/font-hywh/hywh.7z"> <button class="responsive small elevate"> <i>folder_zip</i> 汉仪文黑.7z</button> </a>
+			</div>
+		</details>
+	</article>
+
+	<article class="blur">
+		<details>
+			<summary class="none">
+				<div class="row">
+					<div class="max">
+						<h5 class="text-shadow">Source Code Pro</h5>
+						<div>英文, 编程字体</div>
+					</div>
+					<i>arrow_drop_down</i>
+				</div>
+			</summary>
+
+			<div class="medium-space"></div>
+
+			<a class="chip medium-elevate small primary" href="https://github.com/adobe-fonts/source-code-pro" target="_blank">
+				<i class="small">home</i>
+				<span>官网</span>
+			</a>
+
+			<a class="chip medium-elevate small" href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL" target="_blank">
+				<i class="small">credit_card</i>
+				<span>开源 / SIL 开源字体许可</span>
+			</a>
+
+			<h4>标签引入</h4>
+
+			<code lang="html" ref="domSCPUsage"></code>
+
+			<h4>直接下载</h4>
+
+			<div>单独下载:</div>
+
+			<div class="grid">
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-Black.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> Black </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-BlackIt.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> BlackIt </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-Bold.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> Bold </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-BoldIt.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> BoldIt </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-ExtraLight.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> ExtraLight </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-ExtraLightIt.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> ExtraLightIt </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-It.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> It </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-Light.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> Light </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-LightIt.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> LightIt </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-Medium.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> Medium </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-MediumIt.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> MediumIt </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-Regular.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> Regular </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-Semibold.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> Semibold </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-source-code-pro/TTF/SourceCodePro-SemiboldIt.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> SemiboldIt </button> </a>
+			</div>
+
+			<div class="space"></div>
+
+			<div>打包下载所有:</div>
+
+			<div class="grid">
+				<a class="s12 m12 l4" href="/astatine-gear/font-source-code-pro/TTF-source-code-pro-2.042R-u_1.062R-i.zip"> <button class="responsive small elevate"> <i>folder_zip</i> Source Code Pro 2.042R u 1.062R i.zip</button> </a>
+			</div>
+		</details>
+	</article>
+
+	<article class="blur">
+		<details>
+			<summary class="none">
+				<div class="row">
+					<div class="max">
+						<h5 class="text-shadow">Cabin</h5>
+						<div>英文, 通用字体</div>
+					</div>
+					<i>arrow_drop_down</i>
+				</div>
+			</summary>
+
+			<div class="medium-space"></div>
+
+			<a class="chip medium-elevate small primary" href="https://fonts.google.com/specimen/Cabin?query=cabin" target="_blank">
+				<i class="small">home</i>
+				<span>官网</span>
+			</a>
+
+			<a class="chip medium-elevate small" href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL" target="_blank">
+				<i class="small">credit_card</i>
+				<span>开源 / SIL 开源字体许可</span>
+			</a>
+
+			<h4>标签引入</h4>
+
+			<code lang="html" ref="domCabinUsage"></code>
+
+			<h4>直接下载</h4>
+
+			<div>单独下载:</div>
+
+			<div class="grid">
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-400-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-400-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-400-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-400-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-500-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-500-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-500-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-500-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-600-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-600-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-600-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-600-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-700-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-700-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-700-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-700-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-400-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-400-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-400-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-400-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-500-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-500-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-500-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-500-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-600-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-600-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-600-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-600-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-700-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-700-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-latin-ext-700-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> latin-ext-700-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-400-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-400-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-400-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-400-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-500-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-500-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-500-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-500-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-600-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-600-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-600-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-600-normal </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-700-italic.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-700-italic </button> </a>
+				<a class="s4 m4 l2" href="/astatine-gear/font-cabin/ttf/cabin-vietnamese-700-normal.ttf"> <button class="responsive small elevate"> <i>lab_profile</i> vietnamese-700-normal </button> </a>
+			</div>
+
+			<div class="space"></div>
+
+			<div>打包下载所有:</div>
+
+			<div class="grid">
+				<a class="s12 m12 l4" href="/astatine-gear/font-cabin/cabin.zip"> <button class="responsive small elevate"> <i>folder_zip</i> cabin.zip</button> </a>
+			</div>
+		</details>
+	</article>
+
 </div>
 </template>
 
 <script setup>
 import {onMounted, ref} from 'vue'
 import {marked} from 'marked'
-import DC from './FontPageDoc'
 
-const docContent = ref(null)
+const domHywhUsage = ref()
+const domSCPUsage = ref()
+const domCabinUsage = ref()
 onMounted(() => {
-	docContent.value = marked.parse(DC)
+	domHywhUsage.value.innerText = `<!-- 在 HTML 中引入 -->
+<link rel="stylesheet" href="${window.location.origin}/astatine-gear/font-hywh.css" />
+
+<!-- 然后在 style 中声明字体 -->
+<!-- 引入下述其一即可 -->
+<!-- (不同版本区别在于字重, 数字越大, 字体越粗) -->
+<style>
+  body { font-family: HYWH35W, HYWH45W, HYWH55S, HYWH65W, HYWH75W, HYWH85W }
+</style>`
+
+	domSCPUsage.value.innerText = `<!-- 在 HTML 中引入 -->
+<link rel="stylesheet" href="${window.location.origin}/astatine-gear/font-source-code-pro.css" />
+
+<!-- 然后在 style 中声明字体 -->
+<!-- 引入下述其一即可 -->
+<style>
+  body { font-family:
+  SourceCodePro-ExtraLight,
+   SourceCodePro-ExtraLightIt.ttf,
+   SourceCodePro-It.ttf,
+   SourceCodePro-Light.ttf,
+   SourceCodePro-LightIt.ttf,
+   SourceCodePro-Medium.ttf,
+   SourceCodePro-MediumIt.ttf,
+   SourceCodePro-Regular.ttf,
+   SourceCodePro-Semibold.ttf,
+   SourceCodePro-SemiboldIt.ttf
+	}
+</style>`
+
+	domCabinUsage.value.innerText = `<!-- 在 HTML 中引入 -->
+<link rel="stylesheet" href="${window.location.origin}/astatine-gear/font-cabin.css" />
+
+<!-- 然后在 style 中声明字体 -->
+<!-- 引入下述其一即可 -->
+<style>
+  body { font-family:
+  cabin-latin-400-italic,
+  cabin-latin-400-normal,
+  cabin-latin-500-italic,
+  cabin-latin-500-normal,
+  cabin-latin-600-italic,
+  cabin-latin-600-normal,
+  cabin-latin-700-italic,
+  cabin-latin-700-normal,
+  cabin-latin-ext-400-italic,
+  cabin-latin-ext-400-normal
+	}
+</style>`
 })
+
 
 </script>
--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -18,16 +18,17 @@ i, div, h5, a, span {
 				Assets Table
 			</span>
 		</p>
-		<p class="">开发过程有些资源 (如各类字体) 找不到合适的 CDN</p>
-		<p class="">于是基于 GitHub Pages 和 Cloudflare Pages</p>
-		<p class="">建立一个项目用于加速资源访问</p>
+		<p class="">开发过程有些资源找不到合适的 CDN</p>
+		<p>(主要是各类字体)</p>
+		<p class="">于是在 GitHub Pages 和 Cloudflare Pages</p>
+		<p class="">建立本项目用于加速资源访问</p>
 		<p class="purple-text">如本站访问速度不佳, 请点击右上角按钮切换访问源</p>
 		<p class="purple-text">(其中自定义域名源一般不需要挂梯子)</p>
 		<p class="green-text">本站提供所有资源<span class="deep-orange-text">仅</span>可用于个人非商用用途</p>
 		<div class="space"></div>
 		<nav class="center-align">
 			<a href="https://github.com/FirokOtaku/astatine-gear" target="_blank">
-				<button class="tertiary round">
+				<button class="tertiary round elevate">
 					<i class="medium-text">code</i>
 					GitHub
 
@@ -38,7 +39,7 @@ i, div, h5, a, span {
 				</button>
 			</a>
 
-			<button class="primary round" @click="$emit('sp', 'font')">
+			<button class="primary round elevate" @click="$emit('sp', 'font')">
 				<i class="medium-text">text_fields</i>
 				字体资源
 
@@ -49,7 +50,7 @@ i, div, h5, a, span {
 		</nav>
 
 		<div class="large-space"></div>
-		<table class="no-space">
+		<table>
 			<tbody>
 			<tr v-for="changelog in listChangelog" :key="changelog.date">
 				<td>{{ changelog.date }}</td>
@@ -68,7 +69,8 @@ import {computed} from "vue";
 defineEmits(['sp'])
 const listChangelog = computed(() => {
 	return [
-		{ date: '2023-05-12', title: '初次更新', desc: `增加字体资源:<br>汉仪文黑` },
+		{ date: '2023-05-14', title: '增加字体资源', desc: `汉仪文黑<br>Cabin` },
+		{ date: '2023-05-12', title: '增加字体资源', desc: `Source Code Pro` },
 	]
 })
 </script>
